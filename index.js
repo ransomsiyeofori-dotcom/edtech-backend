3e5145420929ba8b60f0b190ddf204a4c18722fd
Ransom George <ransomsiyeofori@gmail.com>
2026-05-17 09:25:44 +0100

    initial backend

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://Ransomsiyeofori:Daniel1993@cluster0.v6ugs3k.mongodb.net/?appName=Cluster0")
+.then(() => {
+  console.log("Database connected");
+})
+.catch((error) => {
+  console.log(error);
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { fullName, email, phone, dateOfBirth, password, terms } = req.body;
+    
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+    const newUser = new User({
+      fullName,
+      email,
+      phone,
+      dateOfBirth,
+      password: hashedPassword,
+      terms
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful", user: newUser });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+  fullName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+
+  phone: {
+    type: String,
+    required: true
+  },
+
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+
+  terms: {
+    type: Boolean,
+    required: true
+  }
+
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
