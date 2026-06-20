3af3ef4f15b70b7bdb98f20b3237d9532199b101
Ransom George <ransomsiyeofori@gmail.com>
2026-06-20 10:39:40 +0100

    refactor: move auth logic to controller and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,9 +6,44 @@ const verifyOtp = async (req, res) => {
  
   if(!email || !otp) { return res.status(400).json({ success: false,
                 message: "Email and OTP are required"});}
-   
+  
+  const user = await User.findOne({ email });
+
+if (!user) {
+    return res.status(404).json({
+        success: false,
+        message: "User not found"
+    });
+}
+
+if (Date.now() > user.resetOtpExpires) {
+    return res.status(400).json({
+        success: false,
+        message: "OTP has expired"
+    });
+}
+
+if (otp !== user.resetOtp) {
+    return res.status(400).json({
+        success: false,
+        message: "Invalid OTP"
+    });
+}
+
+user.resetOtp = undefined;
+user.resetOtpExpires = undefined;
+await user.save();
+
+return res.status(200).json({
+    success: true,
+    message: "OTP verified successfully"
+});
  }
-   
+ 
+ catch (error) { return res.status(500).json({ success: false,
+          message: "server error"
+  });
+ }
  
 };
 
